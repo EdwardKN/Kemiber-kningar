ea5e64b008801a66947ccfa8f747de154188142c
Edward <edward.klinghammer@gmail.com>
2023-01-27 15:58:21 +0100

    kod

---
+++ balance.js
@@ -0,0 +1,239 @@
+async function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms))}
+
+function isDigit(s) { return s >= '0' && s <= '9' }
+
+function count(arr, count) { return arr.filter(i => i === count).length }
+
+function isEqual(arr1, arr2) {
+    if (arr1.length != arr2.length) { return false }
+    return arr1.every((item, index) => arr2[index] == item)
+}
+
+function deepCopy(arr) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        res.push([])
+        for (let j = 0; j < arr[i].length; j++) {
+            res[i].push(arr[i][j])
+        }
+    }
+    return res
+}
+
+function lowestCommon(arr) {
+    let most = {}
+    let common = 1
+    arr.forEach(factors => {
+        factors = getPrimeFactors(factors)
+        for (let n of factors) {
+            if (most[n]) {
+                most[n] == most[n] < count(factors, n) ? count(factors, n) : most[n]
+            } else {
+                most[n] = count(factors, n)
+            }
+        }
+    })
+    for (let entry of Object.entries(most)) {
+        common *= Math.pow(entry[0], entry[1])
+    }
+    return common
+}
+
+function getPrimeFactors(n) {
+    if (n == 1) { return [1] }
+    let factors = []
+
+    while (n % 2 == 0) {
+        factors.push(2)
+        n = n >> 1
+    }
+    for (let i = 3; i < Math.sqrt(n); i += 2) {
+        while (n % i == 0) {
+            factors.push(i)
+            n /= i
+        }
+    }
+    if (n != 1) { factors.push(n) }
+    
+    return factors
+}
+
+function divideEquation(equation) {
+    equation = equation.split("=")
+    equation[0] = equation[0].split('+')
+    equation[1] = equation[1].split('+')
+    return equation
+}
+
+function seperateNumbers(list) {
+    let numbers = {}
+    list.forEach((_, index) => _.forEach((item, index2) => {
+        let digits = []
+        let res = ""
+        let cur = ""
+        let curNum = ""
+        for (let char of item) {
+
+            if (isDigit(char)) {
+                curNum += char
+            } else if (char != char.toUpperCase()) {
+                cur += char
+            } else {
+                let num = curNum == "" ? 1 : parseInt(curNum)
+                res += cur.repeat(num) // Uppercase
+                digits.push(num)
+                curNum = ""
+                cur = char
+            }
+        }
+        num = curNum == "" ? 1 : parseInt(curNum)
+        digits.push(num)
+        res += cur.repeat(num)
+        list[index][index2] = res
+        numbers[res] = lowestCommon(digits)
+    }))
+
+    return [list, numbers]
+}
+
+async function bruteForce(strings, amountOfEach) { // Object form
+    let combined = [...strings[0]].concat([...strings[1]]) // Combine
+
+    values = await recursive(combined, Array(combined.length).fill(1), 0, amountOfEach,
+    strings[0].length)
+    return values
+}
+
+function sortObj(obj) {
+    let temp = ""
+    Object.entries(obj).forEach(entry => {
+        temp += entry[0].repeat(entry[1])
+    })
+    return temp.split('').sort()
+}
+
+function isLower(string1, string2, element) {
+    for (let char of element) {
+        if (count(string1, char) < count(string2, char)) {
+            return false
+        }
+    }
+    return true
+}
+
+function oneTooTwo(keys, values, split) {
+    if (keys.length !== values.length) { throw "oneTooTwo"}
+    let a = {}
+    let b = {}
+    for (let i = 0; i < keys.length; i++) {
+        if (i < split) {
+            a[keys[i]] = values[i]
+        } else { b[keys[i]] = values[i] }
+    }
+    return [a, b]
+}
+
+function arrToObj(keys, values) {
+    if (keys.length != values.length) { throw "arrToObj" }
+    let temp = {}
+    for (let i = 0; i < keys.length; i++) {
+        temp[keys[i]] = values[i]
+    }
+    return temp
+}
+
+
+
+
+
+
+
+//array["key"] = value
+
+
+async function recursive(combined, values, depth, max, split) {
+    if (depth == combined.length) { return values }
+    let element = combined[depth]
+    let res = oneTooTwo(combined, values, split)
+    let r = res[0]
+    let p = res[1]
+
+
+    if (isEqual(sortObj(r), sortObj(p))) { return values }
+
+    for (let i = 1; i <= max[element]; i++) {
+
+        values[depth] = i
+        values = await recursive(combined, values, depth+1, max, split)
+        res = oneTooTwo(combined, values, split)
+        r = res[0]
+        p = res[1]
+        if (depth >= split) { // Product
+            if (!isLower(sortObj(r), sortObj(p), combined[depth])) { break }
+        }
+        if (isEqual(sortObj(r), sortObj(p))) { return values }
+
+    }
+    values[depth] = 1
+    return values
+}
+// Paranthasese
+function validateEquation(eq) {
+    let t = eq.split('')
+    if (count(t, '(') != count(t, ')')) { return false }
+
+    let lastP = ""
+    let check = false
+    let prev = ""
+    for (let char of eq) {
+        if (char == '(') { lastP == '(' }
+
+        
+        if (isDigit(char) && ['+', '=', ''].includes(prev)) { return false }   
+        if (char == char.toLowerCase() && (prev == prev.toLowerCase() || prev == '')) { return false } 
+        if (char == ')') {
+            if (lastP == '(') { lastP = ""; check = true } else { return false }
+        }
+        prev = char
+    }
+}
+//let test = "AgNO3+BaCl2=AgCl+Ba(NO3)2"
+//console.log(validateEquation(test))
+
+
+async function calculate(equation) {
+    await sleep(10)
+
+    let sT = performance.now()
+
+    let trimmed = equation.replace(/(\r\n|\t|\n|\r| )/gm, "") // Remove whitespaces
+
+    let divided = divideEquation(trimmed) // Divide equation
+    
+    let numbersSeperated = seperateNumbers( deepCopy(divided) ) //
+    console.log()
+    let strings = numbersSeperated[0] // Divided without numbers
+    let numbers = numbersSeperated[1] // Object with the strings as key and smallesCommonDenominator as value
+
+    let smallest = 2 * lowestCommon(Object.values(numbers)) // Smallest common denominator of all elements
+    
+    Object.entries(numbers).forEach(entry => { // Change numbers value so that there value is smallest common denominator / value
+        numbers[entry[0]] = smallest / entry[1]
+    })
+
+    let output = await bruteForce(strings, numbers) // Bruteforce
+
+    let eN = performance.now()
+
+    let i = 0
+    divided.forEach((_, index1) => {
+        _.forEach((_, index2) => {
+        if (output[i] != 1) divided[index1][index2] = `${output[i]}${divided[index1][index2]}`
+        i++
+    }); divided[index1] = divided[index1].join(' + ')})
+
+    console.log(`The function took ${eN - sT} milliseconds`)
+    return output
+}
+// Fix small letters
+
---
+++ script.js
@@ -0,0 +1,324 @@
+var table;
+
+var rows = {
+    input:"",
+    formel:"",
+    mol:"",
+    massa:"",
+    molmassa:""
+};
+
+var newArray;
+
+var json;
+
+readTextFile("../table.json", function(text){
+    json = JSON.parse(text);
+});
+
+
+function init(){
+    table = document.createElement("table");
+    
+    
+    document.body.appendChild(table);
+}
+
+function updateTable(value){
+    let tmpIndex;
+    let amountArray = [];
+        let tmpIndex2;
+    let numberThing = value.value.length;
+    let startValue = value.value
+
+        for(let i =0; i <value.parentNode.parentNode.children.length; i++){
+            if(value.parentNode.parentNode.children[i] == value.parentNode){
+                tmpIndex = i;
+            }
+
+        }
+        for(let i =0; i <value.parentNode.parentNode.parentNode.children.length; i++){
+            if(value.parentNode.parentNode.parentNode.children[i] == value.parentNode.parentNode){
+                tmpIndex2 = i;
+            }
+
+        }
+        if(value.value === "" || value.value === "0"){
+            updateInput(document.getElementById("formel").value)
+        }
+
+        if(isNumeric(value.value)){
+        
+            if(tmpIndex2 === 1){
+                value.parentNode.parentNode.parentNode.children[2].children[tmpIndex].children[0].setAttribute("readonly","")
+                value.parentNode.parentNode.parentNode.children[2].children[tmpIndex].children[0].value = JSON.parse(value.parentNode.parentNode.parentNode.children[3].children[tmpIndex].innerText)*JSON.parse(value.value)
+            }
+            if(tmpIndex2 === 2){
+                value.parentNode.parentNode.parentNode.children[1].children[tmpIndex].children[0].setAttribute("readonly","")
+                value.parentNode.parentNode.parentNode.children[1].children[tmpIndex].children[0].value = JSON.parse(value.value)/JSON.parse(value.parentNode.parentNode.parentNode.children[3].children[tmpIndex].innerText)
+            }
+            
+                for(let n = 0; n < newArray.length; n++){
+                    if(isNumeric(newArray[n][0])){
+                        amountArray[n] = JSON.parse(newArray[n][0]);
+                    }else{
+                        amountArray[n] = 1;
+                    }
+                    
+
+                }
+                for(let n = 0; n < newArray.length; n++){
+
+                    if(newArray[n][0] !== "+" && newArray[n][0] !== "="){                        
+                        value.parentNode.parentNode.parentNode.children[1].children[n].children[0].value = (amountArray[n]/amountArray[tmpIndex])*JSON.parse(value.parentNode.parentNode.parentNode.children[1].children[tmpIndex].children[0].value)
+                        value.parentNode.parentNode.parentNode.children[2].children[n].children[0].value = JSON.parse(value.parentNode.parentNode.parentNode.children[1].children[n].children[0].value)*JSON.parse(value.parentNode.parentNode.parentNode.children[3].children[n].innerText)
+                        value.parentNode.parentNode.parentNode.children[1].children[n].children[0].setAttribute("readonly","");
+                        value.parentNode.parentNode.parentNode.children[2].children[n].children[0].setAttribute("readonly","");
+                    }
+                }
+            
+    
+        }
+        console.log(startValue.length)
+/*
+    for(let x = 0; x < newArray.length; x++){
+        for(let y = 1; y < 3; y++){
+            if(newArray[x][0] !== "+" && newArray[x][0] !== "="){                        
+                if(table.children[y].children[x].children[0] != value){
+                    if(table.children[y].children[x].children[0].value.includes(".")){
+                        table.children[y].children[x].children[0].value = table.children[y].children[x].children[0].value.substring(0,startValue.replaceAll(".","").length+1);
+                    }else{
+                        table.children[y].children[x].children[0].value = table.children[y].children[x].children[0].value.substring(0,startValue.replaceAll(".","").length);
+                    }
+                    if (table.children[y].children[x].children[0].value.charAt(table.children[y].children[x].children[0].value.length - 1) == '.') {
+                        table.children[y].children[x].children[0].value = table.children[y].children[x].children[0].value.substr(0, table.children[y].children[x].children[0].value.length - 1);
+                      }
+                } else{
+                    value.value = startValue;
+                }
+
+            }
+        }
+    }
+    */
+    value.removeAttribute("readonly")
+}
+
+function updateInput(value){
+
+    document.body.appendChild(document.getElementById("formel"))
+
+    while(table.rows.length > 0) {
+        table.deleteRow(0);
+      }
+
+      Object.entries(rows).forEach(rad => {
+        rad[1] = document.createElement("tr")
+        table.appendChild(rad[1])
+    })
+
+    let newValue = value.split("")
+
+    newValue = newValue.filter(item => !(item == ' '));
+    
+
+    
+
+    var oldNewValue = value.split("").filter(item => !(item == ' '));
+
+    for(let i = 0; i < newValue.length; i++){
+
+        if(isNumeric(newValue[i]) && i>0 && newValue[i-1] != "+" && newValue[i-1] != "="  &&isNumeric(newValue[i-1]) == true ){
+            newValue[i-1] += newValue[i];
+            newValue.splice(i,1)
+            i = 0;
+        }
+    }
+    for(let i = 0; i < newValue.length; i++){
+
+         if(newValue[i] == newValue[i].toLowerCase() && newValue[i] != "+" && newValue[i] != "=" && i>0 && !isNumeric(newValue[i] )){
+             newValue[i-1] += newValue[i];
+             newValue.splice(i,1)
+             i = 0;
+             continue;
+         }else{
+             if(isNumeric(newValue[i]) && i>0 && newValue[i-1] != "+" && newValue[i-1] != "="  &&isNumeric(newValue[i-1]) == false ){
+                 newValue[i-1] = newValue[i-1].repeat(newValue[i])
+                 newValue.splice(i,1)
+                 i = 0;
+             }
+        }
+
+
+    }
+    console.log(newValue)
+
+    
+
+    newArray = split(newValue);
+
+    for(let n = 0; n<split(oldNewValue).length;n++ ){
+        for(let i = 0; i< 5; i++){
+            let td;
+            if(i === 4 && n === 0){
+                td = document.createElement("td");
+                td.setAttribute("colspan",split(oldNewValue).length-1)
+                td.appendChild(document.getElementById("formel"))
+
+                td2 = document.createElement("td");
+                let but = document.createElement("button");
+                but.innerText = "Balance"
+                but.setAttribute("onclick","balance()")
+                td2.appendChild(but)
+                table.children[i].appendChild(td)
+                table.children[i].appendChild(td2)
+            }
+            
+            if(i != 4){
+                td = document.createElement("td");
+            }else if(n > 0){
+                continue;
+            }
+            
+            if(i === 0){
+                split(oldNewValue)[n].forEach(valueThing =>{
+                    td.innerText+=valueThing;
+                })
+            }
+            if(i === 1 && split(oldNewValue)[n][0] !== "+" && split(oldNewValue)[n][0] !== "="){
+                let tmp = document.createElement("input");
+                tmp.setAttribute("onchange","updateTable(this)")
+                tmp.value = "0"
+                td.appendChild(tmp);
+            }
+            if(i === 2 && split(oldNewValue)[n][0] !== "+" && split(oldNewValue)[n][0] !== "="){
+                let tmp = document.createElement("input");
+                tmp.setAttribute("onchange","updateTable(this)")
+                tmp.value = "0"
+
+                td.appendChild(tmp);
+            }
+            if(i === 3 && split(oldNewValue)[n][0] !== "+" && split(oldNewValue)[n][0] !== "="){
+                let tmpValue = 0;
+                deep_value(json,"elements").forEach(element => {
+                    
+                    newArray[n].forEach(valueThing =>{
+
+                        valueThing.replace(/\d+|[A-Z]/g, '~$&').split('~').forEach(valueThing2 => {
+                            if(valueThing2 == (deep_value(element,"symbol"))){
+                                tmpValue+=Math.round(deep_value(element,"atomic_mass")*100)/100
+                            }
+                        });
+                        
+
+                    })
+                })
+                td.innerText = tmpValue
+
+            }
+            
+            table.children[i].appendChild(td)
+        }
+    }
+
+    
+}
+
+init();
+
+function balance(){
+    calculate(document.getElementById("formel").value).then(result =>{
+
+        let newValue = document.getElementById("formel").value.split("")
+
+        newValue = newValue.filter(item => !(item == ' '));
+        newValue = split(newValue)
+
+        for(i = 0; i<result.length*2; i+=2){
+            newValue[i].unshift(JSON.stringify(result[i/2]))
+        }
+        document.getElementById("formel").value = "";
+        newValue.forEach(newValueValue => {
+            newValueValue.forEach(newNewValueValue => {
+                if(newNewValueValue === "+" || newNewValueValue === "="){
+                    document.getElementById("formel").value += " ";
+                }
+                document.getElementById("formel").value += newNewValueValue;
+                if(newNewValueValue === "+" || newNewValueValue === "="){
+                    document.getElementById("formel").value += " ";
+                }
+            })
+        })
+        updateInput(document.getElementById("formel").value);
+    })
+}
+
+
+function deep_value(obj, path){
+    for (var i=0, path=path.split('.'), len=path.length; i<len; i++){
+        obj = obj[path[i]];
+    };
+    return obj;
+};
+
+function countProperties(obj) {
+    var count = 0;
+
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            ++count;
+    }
+
+    return count;
+}
+
+function splitMulti(str, tokens){
+    var tempChar = tokens[0]; 
+    for(var i = 1; i < tokens.length; i++){
+        str = str.split(tokens[i]).join(tempChar);
+    }
+    str = str.split(tempChar);
+    return str
+}
+
+
+const split = (arr) => {
+    const output = [];
+    const win = [];
+    
+    arr.forEach((item) => {
+      if (item === "=" || item === "+") {
+        output.push([...win], [item]);
+        win.length = 0;
+      } else {
+        win.push(item);
+      }
+    });
+    if (win.length > 0) {
+      output.push([...win]);
+    }
+    return output;
+  };
+
+  function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+  }
+
+  function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+function UpperCaseArray(input) {
+    var result = input.replace(/([A-Z]+)/g, ",$1").replace(/^,/, "");
+    return result.split(",");
+}
